3ae82f2297d445e0e310bbe4d2db07de6120e8dc
CHOSSY Lucas <mistermega52@gmail.com>
2022-06-20 16:12:30 +0200

    Delete boom.py

--- boom.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-file = open('temp.txt', 'w')
-out = ''
-for i in range(-6,7):
-    for j in range(-2,3):
-        for k in range(-6,7):
-            file.write(f"summon tnt ~{int(i*3.5)+randint(0,1)} ~{int(j*3.5)+randint(-1,1)} ~{int(k*3.5)+randint(-1,0)}" + " {Fuse:" + f"{randint(0,4)}" +"}" + "\n")
-file.close()
