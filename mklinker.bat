91675ec6dcc21f9e781a64f14337ba671b916b0f
CHOSSY Lucas <mistermega52@gmail.com>
2022-06-20 16:12:46 +0200

    Delete mklinker.bat

--- mklinker.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rd "%1\saves\TADPT\datapacks\The-AdvancenergyDP"
-rd "%1\resourcepacks\The-AdvancenergyRP"
-mklink /d "%1\saves\TADPT\datapacks\The-AdvancenergyDP" "%2\Datapack"
-mklink /d "%1\resourcepacks\The-AdvancenergyRP" "%2\Resourcespack"
