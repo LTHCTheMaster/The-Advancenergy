db41a98a79c54929cc203dea6876de5a4d3216a3
CHOSSY Lucas <mistermega52@gmail.com>
2022-08-19 20:12:08 +0200

    Fix an error on a folder name

--- mklinker.bat
+++ mklinker.bat
@@ -2,4 +2,4 @@
 rd "%1\saves\TADPT\datapacks\The-AdvancenergyDP"
 rd "%1\resourcepacks\The-AdvancenergyRP"
 mklink /d "%1\saves\TADPT\datapacks\The-AdvancenergyDP" "%2\Datapack"
-mklink /d "%1\resourcepacks\The-AdvancenergyRP" "%2\Resourcespack"
+mklink /d "%1\resourcepacks\The-AdvancenergyRP" "%2\Resourcepack"
