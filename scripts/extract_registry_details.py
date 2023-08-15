f40945aecc4cbd0eac98429ab3028e4dcc5380f1
CHOSSY Lucas <mistermega52@gmail.com>
2023-08-16 01:22:45 +0200

    better script

--- scripts/extract_registry_details.py
+++ scripts/extract_registry_details.py
@@ -1,27 +1,50 @@
 from os import getcwd, path as pt
 
-def contains(values: tuple[str], tocheck: str) -> bool:
+# Check something
+def contains(values: tuple[str] | list[str], tocheck: str) -> bool:
     for i in values:
         if i in tocheck:
             return True
     return False
 
+# Create the material dict
+def materials(main: dict, can_correspond: list[dict]) -> dict:
+    tmp = dict()
+    if "raw" in main["traits"]:
+        tmp["raw"] = main.copy()
+        tmp["raw_block"] = can_correspond[0].copy()
+    elif "ingot" in main["traits"]:
+        tmp["ingot"] = main.copy()
+        if "nugget" in can_correspond[0]["traits"]:
+            tmp["block"] = can_correspond[1].copy()
+            tmp["nugget"] = can_correspond[0].copy()
+        elif "block" in can_correspond[0]["traits"]:
+            tmp["block"] = can_correspond[0].copy()
+            tmp["nugget"] = can_correspond[1].copy()
+    elif "dust" in main["traits"]:
+        tmp["dust"] = main.copy()
+        tmp["block"] = can_correspond[0].copy()
+    return tmp
+
+# Define path
 MYPATH = pt.relpath('/'.join([i for i in getcwd().replace("\\", "/").split("/")[0:-1]])) + "/Datapack/data/lthc.advancenergy/functions/init/_intern/sub_parts/parts/registry/"
 PATHS = (
     MYPATH + "items.mcfunction",
     MYPATH + "blocks.mcfunction"
 )
 
-materials = []
+# Extract a simple material list
+materials_simple: list[dict] = []
 for i in PATHS:
     with open(i, 'r') as register:
         for zeta in register.readlines():
             try:
                 id_ = zeta.split('tag:{ctc:{id:"')[1].split('",from:"')[0]
+                if id_ == "ash_ingot": continue # Known skippable case
                 from_ = zeta.split('",from:"')[1].split(',traits:{')[0]
                 traits_ = zeta.split(',traits:{')[1].split('}},')[0]
                 if contains(("raw", "ingot", "nugget", "packed", "dust"), traits_):
-                    materials.append({
+                    materials_simple.append({
                         "id": id_,
                         "from": from_,
                         "traits": traits_
@@ -30,7 +53,31 @@ for i in PATHS:
                 pass
         register.close()
 
+# Link materials
+materials_links = []
+for j, i in enumerate(materials_simple):
+    can_correspond = []
+    found = False
+    if contains(("block", "nugget"), i["traits"]): continue # skip useless loop by avoiding already know case to skip
+    for g, h in enumerate(materials_simple):
+        if g == j: continue # Don't link to himself
+        if i["id"].replace("_ingot", "").replace("raw_", "") in h["id"]:
+            if "raw" in i["traits"]:
+                if "block" in h["traits"]:
+                    found = True
+                    can_correspond.append(h.copy())
+            elif "ingot" in i["traits"]:
+                if contains(("block", "nugget"), h["traits"]) and not "raw" in h["traits"]:
+                    found = True
+                    can_correspond.append(h.copy())
+            elif "dust" in i["traits"]:
+                if "block" in h["traits"]:
+                    found = True
+                    can_correspond.append(h.copy())
+    if found: materials_links.append(materials(i, can_correspond))
+
+# Save all details
 REFPATH = pt.relpath('/'.join([i for i in getcwd().replace("\\", "/").split("/")[0:-1]])) + '/references/material_list.txt'
 file = open(REFPATH, 'w')
-file.write('\n'.join(str(i) for i in materials))
+file.write('\n'.join(str(i) for i in materials_links))
 file.close()
